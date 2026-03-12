c57808cfcd5b19aed2ff0f4417d8c0ecdfea7239
J233-human <johnzhou1119@gmail.com>
2026-03-11 21:52:12 -0700

    Early GUI and Background
    Adjusted image scale and size.
    Added two frames for background (future chat) GUI.
    Polished previous chat feature for a basic user input textbox.

--- GUI233.py
+++ GUI233.py
@@ -1,61 +1,77 @@
-from tkinter import *
+import tkinter as tk
+from tkinter import ttk
 from urllib.request import urlopen
 from PIL import Image, ImageTk
 from io import BytesIO
 import webbrowser
 import os
 
-root = Tk()
-root.geometry('1470x956')
-root.title('tunnel')
+# Colors (Can be changed in the futurel; just here for placeholder)
+BANNERBG = "#131415"
+PROFILEBG = "#1B1C1E"
+CHATBG = "#2C2E31"
+TEXTBG = "#32363B"
+SELECTBG = "#6C727C"
 
-root.configure(bg="lightgray")
-# root.columnconfigure(5, weight=1)
-# root.columnconfigure(4, weight=1)
-# root.columnconfigure(3, weight=1)
-# root.columnconfigure(2, weight=1)
-# root.columnconfigure(1, weight=1)
-root.columnconfigure(0, weight=1)
-# root.rowconfigure(7, weight=1)
-# root.rowconfigure(6, weight=1)
-# root.rowconfigure(5, weight=1)
-# root.rowconfigure(4, weight=1)
-root.rowconfigure(3, weight=1)
-root.rowconfigure(2, weight=1)
-root.rowconfigure(1, weight=1)
-root.rowconfigure(0, weight=3)
-
+# Commands
 def onClick():
-    input = txtbox.get()
-    if input == 'meshmilk':
-        label.configure(text = 'meshmilk!!!')
-    else:
-        result = f'You inputted {input}.'
-        label.configure(text = result)
+    input = textbox.get()
+    result = f'You inputted {input}'
+    message.configure(text = result)
 
-img1link = "https://raw.githubusercontent.com/sevdentries/tunnelnet/refs/heads/frontend/asset/computerBackground.png"
-
+# Image loading
+bgimglink = "https://raw.githubusercontent.com/sevdentries/tunnelnet/refs/heads/frontend/asset/computerBackground.png"
 try:
-    with urlopen(img1link) as image:
+    with urlopen(bgimglink) as image:
         img1data = BytesIO(image.read())
 except Exception as lerror: 
     print("Fetch logo failed", str(lerror))
 
-resized_img1 = Image.open(img1data)
-resized_img1 = resized_img1.resize((1470, 478))
+# Root start
+root = tk.Tk()
+root.geometry('1470x956')
+root.title('Tunnelnet')
+root.configure(bg="lightgray")
+root.columnconfigure(2, weight=1)
+root.columnconfigure(1, weight=1)
+root.columnconfigure(0, weight=1)
+root.rowconfigure(2, weight=0)
+root.rowconfigure(1, weight=1) # only row 1 can fill 
+root.rowconfigure(0, weight=0)
 
-img1 = ImageTk.PhotoImage(resized_img1)
+# Images variables
+resized_bgimg = Image.open(img1data)
+resized_bgimg = resized_bgimg.resize((1470, 200))
+bgimg = ImageTk.PhotoImage(resized_bgimg)
 
-img1label = Label(image=img1, bg='lightgray')
-img1label.grid(column=0, row=0, sticky='n')
+# Images
+bgimglabel = tk.Label(image=bgimg, bg='lightgray', border=0)
+bgimglabel.grid(column=0, row=0, sticky='n')
 
-label = Label(root, text='Type something', bg='lightgray')
-label.grid(column=0, row=1, sticky='n')
+# Profile frame (all of left) 
+profileframe = tk.Frame(root, bg=PROFILEBG, width=367.5)
+profileframe.grid(column=0, row=1, sticky='wns')
+profileframe.grid_propagate(False) # stops frame from disappearing
 
-txtbox = Entry(root, width=10)
-txtbox.grid(column=0, row=2, sticky='n')
+# Chat frame (all of right) 
+mainchatframe = tk.Frame(root, bg=CHATBG, width=1102.5)
+mainchatframe.grid(column=0, row=1, sticky='ens')
+mainchatframe.rowconfigure(0, weight=1) # for the chat (fills to fit)
+mainchatframe.rowconfigure(1, weight=0) # for the inputframe (fixed width)
+mainchatframe.grid_propagate(False) # stops frame from disappearing
 
-startbtn = Button(root, text='Start', command=onClick)
-startbtn.grid(column=0, row=3, sticky='n')
+# Input frame (bottom-right) 
+inputframe = tk.Frame(mainchatframe, bg=TEXTBG, width=1102.5, height=20)
+inputframe.grid(column=0, row=1, sticky='es')
+
+# Textbox and send button
+textbox = tk.Entry(inputframe, width=112, bg=TEXTBG, insertbackground='white', selectforeground=TEXTBG, fg='white')
+textbox.grid(column=0, row=0, sticky='es', padx = (5, 5), pady=10)
+sendbtn = tk.Button(inputframe, text='Send', bg=TEXTBG, fg=TEXTBG, command=onClick)
+sendbtn.grid(column=1, row=0, pady=10, padx=(0,5))
+
+# temporary testing code
+message = tk.Label(mainchatframe, text='Send a message to update me!', bg='blue', fg='white')
+message.grid(column=0, row=0)
 
 root.mainloop()

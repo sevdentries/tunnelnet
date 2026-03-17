5a54327b06afbc58e8593718d501764551624011
sev-creates <altonshum@gmail.com>
2026-03-16 22:34:20 -0700

    more stuffes
    disabled os checking

--- tunnelnet.py
+++ tunnelnet.py
@@ -4,6 +4,7 @@ import shlex
 import subprocess
 import threading
 import queue
+import warnings
 from pathlib import Path
 from tkinter import *
 from tkinter import ttk
@@ -95,82 +96,86 @@ def listdevices(apikey, tailnet = "-"):
 def getkeystatus(apikey):
     pass
 
-
-cmd_queue = queue.Queue()
-thread = threading.Thread(target=bash_worker, daemon=True)
-thread.start()
-
+#BASH THREAD STARTER
 if system == "Linux":
-    #ROOT CONFIGS
-    root = Tk()
-    root.geometry("300x150+200+200")
-    root.title("tunnelNET: Login")
-    root.resizable(False,False)
-    for acol in range(3):
-        root.columnconfigure(acol, weight=1)
-    for brow in range(8):
-        root.rowconfigure(brow, weight=1)
+    cmd_queue = queue.Queue()
+    thread = threading.Thread(target=bash_worker, daemon=True)
+    thread.start()
+else:
+    warnings.warn("bash worker thread skipped, login will not work! (use \"testing\" in password to bypass)")
 
-    #MAINWINDOW CONFIGS
-    main = Toplevel(root)
-    main.title("tunnelNET")
-    main.geometry("600x400+200+200")
-    #10rows 10columns
-    for rwcl in range(10):
-        main.rowconfigure(rwcl, weight=1)
-        main.columnconfigure(rwcl, weight=1)
-        
-    main.withdraw()
-    #ELEMENTS
-    initialize = ttk.Notebook(root)
+#ROOT CONFIGS
+root = Tk()
+root.geometry("300x150+200+200")
+root.title("tunnelNET: Login")
+root.resizable(False,False)
+for acol in range(3):
+    root.columnconfigure(acol, weight=1)
+for brow in range(8):
+    root.rowconfigure(brow, weight=1)
 
-    jointab = Frame(initialize)
-    joinlabel = Label(jointab, text="Welcome to tunnelNET!")
-    joinlabel2 = Label(jointab, text="Please enter your join key (tskey-auth):")
-    joinentry = Entry(jointab)
-    joinbutton = Button(jointab, text="Connect", command=lambda:print("yeah yeah ill work on this later"))
+#MAINWINDOW CONFIGS
+main = Toplevel(root)
+main.title("tunnelNET")
+main.geometry("600x400+200+200")
+#10rows 10columns
+for rwcl in range(10):
+    main.rowconfigure(rwcl, weight=1)
+    main.columnconfigure(rwcl, weight=1)
+    
+main.withdraw()
+#ELEMENTS
+initialize = ttk.Notebook(root)
 
-    joinlabel.grid(column=1, row=0 ,sticky=NSEW)
-    joinlabel2.grid(column=1, row=1 ,sticky=NSEW)
-    joinentry.grid(column=1, row=2 ,sticky=EW)
-    joinbutton.grid(column=1, row=3 ,sticky=NSEW)
+jointab = Frame(initialize)
+joinlabel = Label(jointab, text="Welcome to tunnelNET!")
+joinlabel2 = Label(jointab, text="Please enter your join key (tskey-auth):")
+joinentry = Entry(jointab)
+joinbutton = Button(jointab, text="Connect", command=lambda:warnings.warn("yeah yeah ill work on this later"))
 
-    logintab = Frame(initialize)
-    introlabel = Label(logintab, text="Welcome to tunnelNET!")
-    loginlabel = Label(logintab, text="Login (OAuth ID): ")
-    passlabel = Label(logintab, text="Password (OAuth Secret)")
-    loginentry = Entry(logintab)
-    passentry = Entry(logintab)
-    loginbutton = Button(logintab, text="Login", command=login)
+joinlabel.grid(column=1, row=0 ,sticky=NSEW)
+joinlabel2.grid(column=1, row=1 ,sticky=NSEW)
+joinentry.grid(column=1, row=2 ,sticky=EW)
+joinbutton.grid(column=1, row=3 ,sticky=NSEW)
 
-    for bcol in range(3):
-        logintab.columnconfigure(bcol, weight=1)
-        jointab.columnconfigure(bcol, weight=1)
-    for crow in range(8):
-        logintab.rowconfigure(crow, weight=1)
-        jointab.rowconfigure(crow, weight=1)
+logintab = Frame(initialize)
+introlabel = Label(logintab, text="Welcome to tunnelNET!")
+loginlabel = Label(logintab, text="Login (OAuth ID): ")
+passlabel = Label(logintab, text="Password (OAuth Secret)")
+loginentry = Entry(logintab)
+passentry = Entry(logintab)
+loginbutton = Button(logintab, text="Login", command=login)
 
-    initialize.add(logintab, text="Login")
-    initialize.add(jointab, text="Join")
+for bcol in range(3):
+    logintab.columnconfigure(bcol, weight=1)
+    jointab.columnconfigure(bcol, weight=1)
+for crow in range(8):
+    logintab.rowconfigure(crow, weight=1)
+    jointab.rowconfigure(crow, weight=1)
 
-    introlabel.grid(column=1, row=0 ,sticky=NSEW)
-    loginlabel.grid(column=1, row=1, sticky = NSEW)
-    loginentry.grid(column=1,row=2, sticky=EW)
-    passlabel.grid(column=1, row=3, sticky=NSEW)
-    passentry.grid(column=1, row=4, sticky=EW)
-    loginbutton.grid(column=1, row=5, sticky=NSEW)
+initialize.add(logintab, text="Login")
+initialize.add(jointab, text="Join")
 
-    initialize.grid(row=0,column=0,rowspan=8,columnspan=3, sticky=NSEW)
+introlabel.grid(column=1, row=0 ,sticky=NSEW)
+loginlabel.grid(column=1, row=1, sticky = NSEW)
+loginentry.grid(column=1,row=2, sticky=EW)
+passlabel.grid(column=1, row=3, sticky=NSEW)
+passentry.grid(column=1, row=4, sticky=EW)
+loginbutton.grid(column=1, row=5, sticky=NSEW)
 
-    try:
-        with open(USERSAVEDIR, encoding="utf-8") as dingus:
-            usersave = dingus.read()
-            if usersave == "":
-                print("Nothing found in usersave, skipping...")
-            else:
-                loginentry.insert(0, usersave)
-    except FileNotFoundError:
-        print("usersave file not found, program confused. skipping...")
-    except Exception as error:
-        print(error)
+initialize.grid(row=0,column=0,rowspan=8,columnspan=3, sticky=NSEW)
+
+try:
+    with open(USERSAVEDIR, encoding="utf-8") as dingus:
+        usersave = dingus.read()
+        if usersave == "":
+            print("Nothing found in usersave, skipping...")
+        else:
+            loginentry.insert(0, usersave)
+except FileNotFoundError:
+    print("usersave file not found, program confused. skipping...")
+except Exception as error:
+    print(error)
+
+
 root.mainloop()

ca951425ba8bbe84d30c400403c27ebe52ed8403
J233-human <johnzhou1119@gmail.com>
2026-03-17 15:18:35 -0700

    Added Notebook With Early Texting
    Added the notebook/frame code from Discord.
    
    Integrated the frame with previous commit's chatframe and textbook/sendMessage function
    
    Created a test file with original raw code; can also be used in the future as another generic testing file.

--- GUI233.py
+++ GUI233.py
@@ -4,7 +4,7 @@ from urllib.request import urlopen
 import webbrowser
 import os
 
-# Colors (Can be changed in the futurel; just here for placeholder)
+# Colors (Can be changed in the future; just here for placeholder)
 BANNERBG = "#131415"
 PROFILEBG = "#1B1C1E"
 CHATBG = "#2C2E31"
@@ -12,10 +12,15 @@ TEXTBG = "#32363B"
 SELECTBG = "#6C727C"
 
 # Commands
-def onClick():
-    input = textbox.get()
-    result = f'You inputted {input}'
-    message.configure(text = result)
+def sendMessage():
+    message = textbox.get().strip()
+    if message == "": # checks message content, then stops empty spaces from being sent
+        textbox.delete(0, tk.END) # deletes entry text after enter
+    else:
+        current_tab = chattab.nametowidget(chattab.select())
+        entrytextlabel = tk.Label(current_tab, text=message, anchor="w")
+        entrytextlabel.grid(column=0, sticky='w')
+        textbox.delete(0, tk.END)
 
 # Image loading
 bgimglink = 'https://raw.githubusercontent.com/sevdentries/tunnelnet/refs/heads/frontend/Asset/computerBackground.png'
@@ -41,7 +46,6 @@ root.rowconfigure(1, weight=1) # only row 1 can fill
 root.rowconfigure(0, weight=0)
 
 # Images variables
-
 bgimgdata = tk.PhotoImage(data=bgimgraw) # code for file with any dimensions.
 bgimg = bgimgdata.zoom(1,5)
 bgimg = bgimgdata.subsample(1,6)
@@ -50,7 +54,7 @@ bgimg = bgimgdata.subsample(1,6)
 logoimgdata = tk.PhotoImage(data=logoimgraw)
 logoimg = logoimgdata.subsample(5,5)
 
-# Images
+# Background Image
 bgimglabel = tk.Label(image=bgimg, bg='lightgray', border=0)
 bgimglabel.grid(column=0, row=0, sticky='n')
 
@@ -73,6 +77,26 @@ mainchatframe.rowconfigure(0, weight=1) # for the chat (fills to fit)
 mainchatframe.rowconfigure(1, weight=0) # for the inputframe (fixed width)
 mainchatframe.grid_propagate(False) # stops frame from disappearing
 
+# Chat frame notebook
+chattab = ttk.Notebook(mainchatframe)
+chattab.grid_propagate(False)
+
+chatframe1 = tk.Frame(chattab, width=750, height=500)
+chatframe2 = tk.Frame(chattab, width=750, height=500)
+chatframe3 = tk.Frame(chattab, width=750, height=500)
+chatframe1.grid_columnconfigure(0, weight=1)
+chatframe2.grid_columnconfigure(0, weight=1)
+chatframe3.grid_columnconfigure(0, weight=1)
+chatframe1.grid_propagate(False)
+chatframe2.grid_propagate(False)
+chatframe3.grid_propagate(False)
+
+chattab.add(chatframe1, text="F1")
+chattab.add(chatframe2, text="F2")
+chattab.add(chatframe3, text="F3")
+
+chattab.grid(column=0, row=0)
+
 # Input frame (bottom-right) 
 inputframe = tk.Frame(mainchatframe, bg=TEXTBG, width=1102.5, height=20)
 inputframe.grid(column=0, row=1, sticky='es')
@@ -80,11 +104,11 @@ inputframe.grid(column=0, row=1, sticky='es')
 # Textbox and send button
 textbox = tk.Entry(inputframe, width=112, bg=TEXTBG, insertbackground='white', selectforeground=TEXTBG, fg='white')
 textbox.grid(column=0, row=0, sticky='es', padx = (5, 5), pady=10)
-sendbtn = tk.Button(inputframe, text='Send', bg=TEXTBG, fg=TEXTBG, command=onClick)
+textbox.bind("<Return>", lambda event:sendMessage()) # allows pressing enter to chat
+sendbtn = tk.Button(inputframe, text='Send', bg=TEXTBG, fg=TEXTBG, command=sendMessage)
 sendbtn.grid(column=1, row=0, pady=10, padx=(0,5))
 
 # Temporary testing code
-message = tk.Label(mainchatframe, text='Send a message to update me!', bg='blue', fg='white')
-message.grid(column=0, row=0)
+# N/A
 
 root.mainloop()

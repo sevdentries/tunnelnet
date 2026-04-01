8d1d70ef0972c90da77b6fe2356fcf7f920f54bb
J233-human <johnzhou1119@gmail.com>
2026-04-01 13:24:19 -0700

    Pulled frontend-GUI

--- GUI233.py
+++ GUI233.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import *
 from urllib.request import urlopen
 import webbrowser
 import os
@@ -10,6 +11,10 @@ PROFILEBG = "#1B1C1E"
 CHATBG = "#2C2E31"
 TEXTBG = "#32363B"
 SELECTBG = "#6C727C"
+SERVERBG = "#202124"
+
+# Variables
+# placeholder
 
 # Commands
 def sendMessage():
@@ -23,8 +28,8 @@ def sendMessage():
         textbox.delete(0, tk.END)
 
 # Image loading
-bgimglink = 'https://raw.githubusercontent.com/sevdentries/tunnelnet/refs/heads/frontend/Assets/computerBackground.png'
-logoimglink = 'https://raw.githubusercontent.com/sevdentries/tunnelnet/refs/heads/frontend/Assets/tunnel.png'
+bgimglink = 'https://raw.githubusercontent.com/sevdentries/tunnelnet/refs/heads/main/Assets/computerBackground.png'
+logoimglink = 'https://raw.githubusercontent.com/sevdentries/tunnelnet/refs/heads/main/Assets/tunnel.png'
 try:
     with urlopen(bgimglink) as img1:
         bgimgraw = img1.read()
@@ -38,7 +43,7 @@ main = tk.Tk()
 main.geometry('600x400+100+100')
 main.title('Tunnelnet')
 main.configure(bg="lightgray")
-main.columnconfigure(1, weight=3) # for mainchat
+main.columnconfigure(1, weight=8) # for mainchat
 main.columnconfigure(0, weight=1) # for profile
 main.rowconfigure(1, weight=10) # for chat
 main.rowconfigure(0, weight=0) # for computer background image
@@ -47,7 +52,6 @@ main.rowconfigure(0, weight=0) # for computer background image
 bgimgdata = tk.PhotoImage(data=bgimgraw) # code for file with any dimensions.
 bgimg = bgimgdata.zoom(1,1)
 bgimg = bgimgdata.subsample(1,5)
-# bgimg = tk.PhotoImage(file='link') # code for file with correct dimensions.
 
 logoimgdata = tk.PhotoImage(data=logoimgraw)
 logoimg = logoimgdata.subsample(5,5)
@@ -61,11 +65,20 @@ profileframe = tk.Frame(main, bg=PROFILEBG)
 profileframe.grid(column=0, row=1, sticky='nsew')
 profileframe.grid_columnconfigure(0, weight=0)
 profileframe.grid_columnconfigure(1, weight=1)
+profileframe.grid_columnconfigure(2, weight=0)
 profileframe.grid_rowconfigure(0, weight=0)
 profileframe.grid_rowconfigure(1, weight=1)
+profileframe.grid_rowconfigure(2, weight=0)
+profileframe.grid_rowconfigure(3, weight=0)
 
 logoimglabel = tk.Label(profileframe, image=logoimg, border=0)
 logoimglabel.grid(column=0, row=0, padx=20, pady=20)
+
+namelabel = tk.Label(profileframe, text="Tunnelnet", font=("Arial", 20))
+namelabel.grid(column=1, row=0)
+
+serverframe = tk.Frame(profileframe, bg=SERVERBG)
+serverframe.grid(column=0, row=1, columnspan=3, sticky='nsew')
 
 # Chat frame (all of right) 
 mainchatframe = tk.Frame(main, bg=CHATBG)
@@ -74,6 +87,7 @@ mainchatframe.columnconfigure(0, weight=1)
 mainchatframe.columnconfigure(1, weight=1)
 mainchatframe.rowconfigure(0, weight=1) # for the chat to fill
 mainchatframe.rowconfigure(1, weight=0) # for the inputframe to remain same size
+mainchatframe.grid_propagate(False)
 
 # Chat frame notebook
 chattab = ttk.Notebook(mainchatframe)
@@ -98,11 +112,19 @@ inputframe.rowconfigure(0, weight=1)
 inputframe.grid(column=0, row=1, columnspan=2, sticky='nsew')
 
 # Textbox and send button
-textbox = tk.Entry(inputframe, bg=TEXTBG, insertbackground='white', selectforeground=TEXTBG, fg='white')
+textbox = tk.Entry(inputframe, bg=TEXTBG, insertbackground='white', selectbackground='white', fg='white')
 textbox.grid(column=0, row=0, sticky='ew', padx=5, pady=10)
 textbox.bind("<Return>", lambda event:sendMessage()) # allows pressing enter to chat
 sendbtn = tk.Button(inputframe, text='Send', bg=TEXTBG, fg=TEXTBG, command=sendMessage)
 sendbtn.grid(column=1, row=0, sticky='ew', pady=10, padx=(0,5))
+
+# Other Functions
+def resize_text(event):
+    # Calculate new font size based on window width
+    if event.widget == main:
+        new_size = max(20, int(event.width / 35))
+        namelabel.config(font=("Arial", new_size))
+main.bind("<Configure>", resize_text) # allows the resize gets detected
 
 # Temporary testing code
 # N/A
